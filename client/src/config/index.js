183dec8c19a89428b650a5d2cf34f776d1f2f302
Sonne <carlosh.mitma@gmail.com>
2020-07-14 00:56:46 -0500

    shit env

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,5 +1,5 @@
 const endpoint = process.browser ? process.env.BROWSER_API_GQL : process.env.SERVER_API_GQL;
 console.log(process.browser, process.env.SERVER_API_GQL, process.env.BROWSER_API_GQL, process.env);
 export default {
-	endpoint
+	endpoint: process.env.NODE_ENV == 'production' ? 'http://apiblog.sonnemon.com' : endpoint
 };
