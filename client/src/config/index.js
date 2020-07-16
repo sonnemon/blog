90833ec5960ab903d69a845aaf7b2bad479b4a7b
Carlos Huarcaya Mitma <chuarcaya@quanticotrends.com>
2020-07-15 21:23:10 -0500

    xd

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,5 +1,12 @@
-const endpoint = process.browser ? process.env.BROWSER_API_GQL : process.env.SERVER_API_GQL;
-console.log(process.browser, process.env.SERVER_API_GQL, process.env.BROWSER_API_GQL);
+const endpoint = process.browser
+  ? process.env.BROWSER_API_GQL
+  : process.env.SERVER_API_GQL;
+console.log(
+  process.browser,
+  process.env.SERVER_API_GQL,
+  process.env.BROWSER_API_GQL,
+  process.env.NODE_ENV
+);
 export default {
-	endpoint
+  endpoint,
 };
