253d42c6dc1535ba55030348593e8c23c54b122a
Carlos Huarcaya Mitma <chuarcaya@quanticotrends.com>
2020-07-15 20:23:50 -0500

    next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-	distDir: 'build',
-	env: {
-		API: process.env.API,
-		SERVER_API_GQL: process.env.SERVER_API_GQL,
-		BROWSER_API_GQL: process.env.BROWSER_API_GQL,
-		HOST: process.env.HOST
-	}
+  distDir: "build",
+  env: {
+    API: process.env.API ? process.env.API : "http://apiblog.sonnemon.com",
+    SERVER_API_GQL: process.env.SERVER_API_GQL
+      ? process.env.SERVER_API_GQL
+      : "http://apiblog.sonnemon.com/graphql",
+    BROWSER_API_GQL: process.env.BROWSER_API_GQL
+      ? process.env.BROWSER_API_GQL
+      : "http://apiblog.sonnemon.com/graphql",
+    HOST: process.env.HOST ? process.env.HOST : "http://blog.sonnemon.com",
+  },
 };
